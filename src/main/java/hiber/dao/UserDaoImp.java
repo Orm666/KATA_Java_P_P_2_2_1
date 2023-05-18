8788112e82a0cbb098b8f3197796e23bba5a16ac
Orm666 <anton.ga4an@yandex.ru>
2023-05-19 01:36:22 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,39 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+   @Override
+   @SuppressWarnings("unchecked")
+   public User search(String model, int series) {
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User where car.model = :paramModel and car.series = :paramSeries");
+      query.setParameter("paramModel", model);
+      query.setParameter("paramSeries", series);
+      return query.getSingleResult();
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+}
